8d9c53d19681559e9911304912582b664518ccc5
arthurgranito <arthurcursotecnico@gmail.com>
2025-01-03 15:53:48 -0300

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/quotes-app'>
+    <BrowserRouter basename='/quotes-website'>
       <Provider>
         <App />
       </Provider>
